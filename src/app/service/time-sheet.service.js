a106e38dd618776579727429e18ef26ecb17de46
Shoji Soma <shoji.soma@sap.com>
2022-10-14 17:16:05 +0900

    ignore `src/app` for lint

--- src/app/service/time-sheet.service.js
+++ src/app/service/time-sheet.service.js
@@ -1,80 +1,77 @@
-sap.ui.define(
-  [
-    'com/sap/team-calendar/service/http',
-    'com/sap/team-calendar/util/converter'
-  ],
-  function (http, converter) {
-    'use strict';
+sap.ui.define([
+  "com/sap/timesheet/service/http",
+  "com/sap/timesheet/util/converter"
+], function (http, converter) {
+  "use strict";
 
-    var host = '';
-    var servicePath = '/odata/v2/TimesheetService';
-    var baseUrl = host + servicePath;
+  var host = "";
+  var servicePath = "/odata/v2/TimesheetService";
+  var baseUrl = host + servicePath;
 
-    // http.setCSRFPrefilter(baseUrl);
+  // http.setCSRFPrefilter(baseUrl);
 
-    return {
-      getPersons: function () {
-        return http.get(baseUrl + '/Person').then(function (response) {
-          return response.hasOwnProperty('d')
-            ? converter.getResults(converter.getD(response))
-            : converter.getValue(response);
+  return {
+    getPersons: function () {
+      return http.get(baseUrl + "/Person").then(function (response) {
+        return response.hasOwnProperty("d")
+          ? converter.getResults(converter.getD(response))
+          : converter.getValue(response);
+      });
+    },
+
+    getAppointments: function () {
+      return http
+        .get(baseUrl + "/TeamCalendar(" + new Date().getFullYear() + ")", {
+          $expand: "appointments",
+          $select: "year,appointments"
+        })
+        .then(function (response) {
+          return response.hasOwnProperty("d")
+            ? converter.getResults(converter.getD(response).appointments)
+            : response.appointments;
         });
-      },
+    },
 
-      getAppointments: function () {
-        return http
-          .get(baseUrl + '/TeamCalendar(' + new Date().getFullYear() + ')', {
-            $expand: 'appointments',
-            $select: 'year,appointments'
-          })
-          .then(function (response) {
-            return response.hasOwnProperty('d')
-              ? converter.getResults(converter.getD(response).appointments)
-              : response.appointments;
-          });
-      },
+    createAppointment: function (appointment, personid) {
+      var startDate = converter.serializeDate(appointment.start);
+      var endDate = converter.serializeDate(appointment.end);
+      var appointmentData = {
+        start_date: startDate,
+        start_time: converter.serializeTime(true),
+        end_date: endDate,
+        end_time: converter.serializeTime(false),
+        title: appointment.title,
+        info: appointment.info,
+        type: "Project",
+        calendar_year: appointment.start.getFullYear(),
+        person_ID: personid,
+        ID: Date.now() + ""
+      };
 
-      createAppointment: function (appointment, personid) {
-        var startDate = converter.serializeDate(appointment.start);
-        var endDate = converter.serializeDate(appointment.end);
-        var appointmentData = {
-          start_date: startDate,
-          start_time: converter.serializeTime(true),
-          end_date: endDate,
-          end_time: converter.serializeTime(false),
-          title: appointment.title,
-          info: appointment.info,
-          type: 'Project',
-          calendar_year: appointment.start.getFullYear(),
-          person_ID: personid,
-          ID: Date.now() + ''
-        };
+      return http
+        .post(baseUrl + "/Appointment", appointmentData)
+        .then(function () {
+          return appointmentData;
+        });
+    },
 
-        return http
-          .post(baseUrl + '/Appointment', appointmentData)
-          .then(function () {
-            return appointmentData;
-          });
-      },
-
-      updateAppointment: function (appointmentid, payload) {
-        if (payload.start) {
-          payload.start_date = converter.serializeDate(payload.start);
-          delete payload.start;
-        }
-        if (payload.end) {
-          payload.end_date = converter.serializeDate(payload.end);
-          delete payload.end;
-        }
-        return http.patch(
-          baseUrl + "/Appointment('" + appointmentid + "')",
-          payload
-        );
-      },
-
-      approveTimeOff: function (appointmentid) {
-        return this.updateAppointment(appointmentid, { status: 'APPROVED' });
+    updateAppointment: function (appointmentid, payload) {
+      if (payload.start) {
+        payload.start_date = converter.serializeDate(payload.start);
+        delete payload.start;
       }
-    };
-  }
-);
+      if (payload.end) {
+        payload.end_date = converter.serializeDate(payload.end);
+        delete payload.end;
+      }
+      return http.patch(
+        baseUrl + "/Appointment('" + appointmentid + "')",
+        payload
+      );
+    },
+
+    approveTimeOff: function (appointmentid) {
+      return this.updateAppointment(appointmentid, { status: "APPROVED" });
+    }
+  };
+});
